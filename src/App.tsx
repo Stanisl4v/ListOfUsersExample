f66159a170217181dc3c500f8284e835b87c43a1
Slavko Stojsavljevic <85332805+Stanisl4v@users.noreply.github.com>
2022-09-07 13:09:38 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+import Employees from './Employees';
+import Footer from './Footer';
+
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Employees />
+      <Footer />
+    </div>
+  )
+}
